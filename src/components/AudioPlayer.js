ec6ffd9687f54b3724b09be82feb3ed844b44988
Astha <asthamaths@gmail.com>
2017-03-02 00:53:45 +0530

    Implemented conditional rendering with change in code format

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -5,21 +5,21 @@ class AudioPlayer extends React.Component{
 	constructor(props){
 		super(props)
 	}
-	render(){
-		const playPauseClass = ClassNames({
-			'fa fa-play': this.props.isPlaying === true ? false : true,
-			'fa fa-pause': this.props.isPlaying === true ? true : false
-		})
+	handleClick = (e) => {
+			console.log(e)
+	}
+
+	renderPlayer(){
 		return(
 			<div className="player" >
 				<button onClick={() => this.props.togglePlay(this.props.howlInstance)}>
-					<i className={playPauseClass}></i>
+					<i className={this.playPauseClass}></i>
 				</button>
 				<div className="player-controls">
 					{this.props.howlInstance &&
 						<div>
 							<span className="time-elapsed">
-								{this.props.howlInstance.pos()}
+								{this.props.howlInstance.seek()}
 							</span>
 							<div className="progress">
 								<div className="progress-filed" value={this.props.howlInstance.seek()}>
@@ -28,16 +28,35 @@ class AudioPlayer extends React.Component{
 							<span>
 								{this.props.howlInstance.duration()}
 							</span>
+							<button data-skip="-10" onClick={(e) => this.handleClick(e)}>10</button>
+							<button data-skip="25" onClick={(e) => this.handleClick(e)}>25</button>
 						</div>
 					}
 				</div>
 			</div>
 		)
 	}
-}
 
-AudioPlayer.defaultProps = {
-	howlInstance: {}
+	renderPlayerOrNot(){
+		if(this.props.howlInstance){
+			console.log(this.props.howlInstance)
+			return this.renderPlayer()
+		} else {
+			console.log(this.props.howlInstance)
+			return <div></div>
+		}
+	}
+	render(){
+		const playPauseClass = ClassNames({
+			'fa fa-play': this.props.isPlaying === true ? false : true,
+			'fa fa-pause': this.props.isPlaying === true ? true : false
+		})
+		return(
+			<div>
+				{() => this.renderPlayerOrNot()}
+			</div>
+		)
+	}
 }
 
 export default AudioPlayer;
